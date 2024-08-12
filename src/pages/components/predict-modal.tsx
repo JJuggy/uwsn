c55aeb3443e3384757ef5cfea4429a5b403b84d0
Amiolemen David <david@piqy.co.uk>
2024-08-12 10:28:24 +0100

    update

--- src/pages/components/predict-modal.tsx
+++ src/pages/components/predict-modal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function PredictModal({ show, onClose, children, header }) {
+function PredictModal({ show, onClose, children, header }: any) {
   return (
     <div className="fixed  inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md mx-4">
